ab157cd4a0834d006befc27bc3a9826d86f260c8
Thorsten Behrens <tbehrens@novell.com>
2011-03-21 01:06:58 +0100

    Added upload facilities to tinbuild
    Tweaked Norbert's daily_push script to be ~platform-agnostic, and
    added option to tinbuild to call that once a day.

---
+++ bin/push_nightlies.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+## subroutines
+usage ()
+{
+	echo "Usage: $0 [options]"
+	echo "Options:"
+	echo "-h          this help"
+	echo "-t <time>   pull time of this checkout"
+	echo "-n <name>   name of this tinderbox"
+	echo "-l <kbps>   bandwidth limit for upload (KBps)"
+}
+
+BUILDER_NAME=
+PULL_TIME=
+BANDWIDTH_LIMIT=20
+while getopts ht:n:l: opt ; do
+	case "$opt" in
+		h) usage; exit ;;
+		t) PULL_TIME="$OPTARG" ;;
+		n) BUILDER_NAME="$OPTARG" ;;
+		l) BANDWIDTH_LIMIT="$OPTARG" ;;
+		?) usage; exit ;;
+	esac
+done
+
+if [ -z "$PULL_TIME" -o -z "$BUILDER_NAME" ] ; then
+	echo "missing argument. syntax $0 -t <git_pull_timestap> -n <tinderbox_name>" 1>&2
+    exit 1;
+fi
+
+if [ ! -d "instsetoo_native" ] ; then
+	echo "current working directory is not, or not a valid bootstrap git repo" 1>&2
+	exit 1;
+fi
+
+CURR_HEAD=$(<".git/HEAD")
+BRANCH="${CURR_HEAD#*/*/}"
+tag="${BRANCH}~${PULL_TIME}"
+ssh upload@gimli.documentfoundation.org "mkdir -p /srv/www/dev-builds.libreoffice.org/daily/${BUILDER_NAME}/${BRANCH}/${PULL_TIME}"
+
+. ./*[Ee]nv.[Ss]et.sh
+cd instsetoo_native
+mkdir ${OUTPATH}/push 2>/dev/null
+
+for file in $(find . -name "*.dmg" -o -name "*.tar.gz" -o -name "*.exe" | grep -v "/push/")
+do
+	target=$(basename $file)
+	target="${tag}_${target}"
+
+	mv $file ${OUTPATH}/push/$target
+done;
+
+rsync --bwlimit=${BANDWIDTH_LIMIT} -avPe ssh ${OUTPATH}/push/* upload@gimli.documentfoundation.org:/srv/www/dev-builds.libreoffice.org/daily/${BUILDER_NAME}/${BRANCH}/${PULL_TIME}/
+if [ "$?" == "0" ] ; then
+	ssh upload@gimli.documentfoundation.org "cd /srv/www/dev-builds.libreoffice.org/daily/${BUILDER_NAME}/${BRANCH}/ && ln -sf ${PULL_TIME} current"
+fi
