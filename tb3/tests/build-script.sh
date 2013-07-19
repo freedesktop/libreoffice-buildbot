49ef0430ac7bc50800d84030fdb326cfa5984b1d
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2013-07-19 20:39:06 +0200

    missing test build script

---
+++ tb3/tests/build-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+echo "building commit $1 from repo $3 in workdir $2 on platform $4 as builder $5."
+true
+# vim: set et sw=4 ts=4:
