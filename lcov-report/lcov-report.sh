f01d6c1c08cc21350f94bb8b121e6e5f4bd6761e
Maarten Hoes <hoes.maarten@gmail.com>
2014-11-09 11:29:37 +0100

    Added a license statement.
    Change-Id: Iedf068824245ad7d9c0c75fad3f92bf7c30fb4e5
    Reviewed-on: https://gerrit.libreoffice.org/12325
    Reviewed-by: Norbert Thiebaud <nthiebaud@gmail.com>
    Tested-by: Norbert Thiebaud <nthiebaud@gmail.com>

--- lcov-report/lcov-report.sh
+++ lcov-report/lcov-report.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
 
 SRC_DIR=/home/buildslave/source/libo-core
 TRACEFILE_DIR=/home/buildslave/lcov
